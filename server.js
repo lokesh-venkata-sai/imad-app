2ac6e142818a7b76f5118c24ad8da5258ba97e28
lokesh-venkata-sai <36584697+lokesh-venkata-sai@users.noreply.github.com>
2018-02-21 17:20:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function (req, res)
 {
-    res.sendFile(createtemplate(articleone));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/artile-two',function (req, res)
