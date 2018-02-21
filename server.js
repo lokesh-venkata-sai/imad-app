993265b3e540245b5f2a2f9f606c85ff98fe7342
lokesh-venkata-sai <36584697+lokesh-venkata-sai@users.noreply.github.com>
2018-02-21 16:28:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one',function(req, res)
+app.get('/article-one',function (req, res)
 {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('article-two',function(req, res)
+app.get('/article-two',function (req, res)
 {
     res.send("article-two is requested and served here");
 });
 
-app.get('article-three',function(req, res)
+app.get('/article-three',function (req, res)
 {
     res.send("article-three is requested and served here");
 });
