26308c3145ac38a52037c850c0d0df355bae7fc7
lokesh-venkata-sai <36584697+lokesh-venkata-sai@users.noreply.github.com>
2018-02-21 23:13:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick=function(){
     request.send(null);
 };
 
-var nameInput =document.getElementById('name');
-var name =nameInput.value;
+
 var submit =document.getElementById('submit_btn');
 submit.onclick = function(){
     var request =new XMLHttpRequest();
@@ -42,6 +41,8 @@ submit.onclick = function(){
             }
         }
     };
+    var nameInput =document.getElementById('name');
+    var name =nameInput.value;
     request.open('GET','http://mamidilsai.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
